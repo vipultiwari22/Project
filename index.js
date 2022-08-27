1f60ae13d39875f42457f0946109dc8f9b8d777f
Vipul Tiwari <104669764+vipultiwari22@users.noreply.github.com>
2022-08-27 18:27:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+    function abc(n){
+     var data=document.getElementsByClassName("content-4");
+     for(i=0;i<data.length;i++){
+     data[i].style.height="0";
+     data[n].style.height="120px";
+     }
+    
+    }
+
+    function xyz(n){
+    var data=document.getElementsByClassName("content-5");
+    for(i=0;i<data.length;i++){
+    data[i].style.display="none";
+    data[n].style.display="block";
+    }
+   
+   }
+    
